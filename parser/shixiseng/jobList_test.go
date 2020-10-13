88636380420d2dd3728d355f5d4d7a8476944687
Chengxu Bian <piensengxv@gmail.com>
2020-10-13 23:59:51 +0800

    revise parseJobListTest

--- parser/shixiseng/jobList_test.go
+++ parser/shixiseng/jobList_test.go
@@ -1,13 +1,13 @@
 package parser
 
 import (
-	"io/ioutil"
+	"job-hunter/fetch"
 	"testing"
 )
 
 func TestParseJobList(t *testing.T) {
 	const urlLen = 20
-	resp, err := ioutil.ReadFile("jobListTest.html")
+	resp, err := fetch.Fetch("https://www.shixiseng.com/interns?page=1&type=intern&keyword=IT%E4%BA%92%E8%81%94%E7%BD%91&area=&months=&days=&degree=&official=&enterprise=&salary=-0&publishTime=&sortType=&city=%E5%85%A8%E5%9B%BD&internExtend=")
 	if err != nil {
 		panic(err)
 	}
